2916983b6eac1ac8d95101ce1710172aee4b92ec
Raghavendra-Mahendrakar <raghukm338@gmail.com>
2022-12-13 18:01:48 +0530

    code push

--- src/main/java/com/example/springcurdexample/controller/ProductController.java
+++ src/main/java/com/example/springcurdexample/controller/ProductController.java
@@ -3,6 +3,9 @@ package com.example.springcurdexample.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,4 +28,30 @@ public class ProductController {
 	public List<Product> addProducts(@RequestBody List<Product> products) {
 		return service.saveProducts(products);
 	}
-}
+	
+	@GetMapping("/products")
+	public List<Product> findAllProducts(){
+		return service.getProdtct();
+	}
+	
+	@GetMapping("/product{id}")
+	public Product findProductById(@PathVariable int id) {
+		return service.getProductById(id);
+	}
+	@GetMapping("/product{name}")
+	public Product findProductByName(@PathVariable String name) {
+		return service.getProductByName(name);
+	}
+	
+	@PostMapping("/update")
+	public Product updateProduct(@RequestBody Product product) {
+		return service.updateProduct(product);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String deleteProduct(@PathVariable int id) {
+		return service.deleteProduct(id);
+	}
+	
+	}
+
